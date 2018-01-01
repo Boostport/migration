06dc9fa2f49592c6c87fe2b30b76b8adc5a8a726
Francis Chuang <francis.chuang@boostport.com>
2018-01-01 14:17:17 +1100

    Try to keep whitespace after parsing to prevent breaking migrations written in whitespace-sensitive languages such as YAML

--- parser/parser.go
+++ parser/parser.go
@@ -21,17 +21,16 @@ type ParsedMigration struct {
 	Statements     []string
 }
 
-func splitStatementsBySemicolon(buf bytes.Buffer) []string {
+func splitStatementsBySemicolon(buf string) []string {
 
-	statements := strings.SplitAfter(buf.String(), ";")
+	statements := strings.SplitAfter(buf, ";")
 
 	for i, statement := range statements {
 		trimmed := strings.TrimSpace(statement)
 
-		if trimmed == "" {
+		if trimmed == "" && i > 0 {
+			statements[i-1] = statements[i-1] + statements[i]
 			statements = statements[:i+copy(statements[i:], statements[i+1:])]
-		} else {
-			statements[i] = trimmed + "\n"
 		}
 	}
 
@@ -46,21 +45,24 @@ func Parse(r io.Reader) (*ParsedMigration, error) {
 	}
 
 	var buf bytes.Buffer
+
 	scanner := bufio.NewScanner(r)
+	scanner.Split(scanLines)
 
 	isFirstLine := true
 
 	for scanner.Scan() {
 
-		line := strings.TrimSpace(scanner.Text())
+		line := scanner.Text()
+		trimmed := strings.TrimSpace(line)
 
-		if strings.HasPrefix(line, sqlCmdPrefix) {
-			option := strings.Replace(line, sqlCmdPrefix, "", -1)
+		if strings.HasPrefix(trimmed, sqlCmdPrefix) {
+			option := strings.Replace(trimmed, sqlCmdPrefix, "", -1)
 
 			switch option {
 			case optionNoTransaction:
 				if !isFirstLine && buf.Len() > 0 {
-					return p, fmt.Errorf("%s%s must be in the first line of the migration.", sqlCmdPrefix, optionNoTransaction)
+					return p, fmt.Errorf("%s%s must be in the first line of the migration", sqlCmdPrefix, optionNoTransaction)
 				}
 
 				p.UseTransaction = false
@@ -68,10 +70,12 @@ func Parse(r io.Reader) (*ParsedMigration, error) {
 			case optionBeginStatement:
 
 				// Add lines encountered before beginning the statement
+				withoutCR := string(dropCR(buf.Bytes()))
+
 				if !p.UseTransaction {
-					p.Statements = append(p.Statements, splitStatementsBySemicolon(buf)...)
+					p.Statements = append(p.Statements, splitStatementsBySemicolon(withoutCR)...)
 				} else {
-					p.Statements = append(p.Statements, buf.String())
+					p.Statements = append(p.Statements, withoutCR)
 				}
 
 				buf.Reset()
@@ -79,13 +83,14 @@ func Parse(r io.Reader) (*ParsedMigration, error) {
 			case optionEndStatement:
 
 				// Add the lines encountered during a statement block as 1 block
-				p.Statements = append(p.Statements, buf.String())
+				p.Statements = append(p.Statements, string(dropCR(buf.Bytes())))
 
 				buf.Reset()
 			}
-		} else if line != "" {
-			if _, err := buf.WriteString(line + "\n"); err != nil {
-				return p, errors.New("Error writing line to buffer")
+		} else {
+
+			if _, err := buf.WriteString(line); err != nil {
+				return p, errors.New("error writing line to buffer")
 			}
 		}
 
@@ -93,13 +98,39 @@ func Parse(r io.Reader) (*ParsedMigration, error) {
 	}
 
 	// If the buffer contains lines, process them
-	if buf.Len() > 0 {
+	if buf.Len() > 0 && strings.TrimSpace(buf.String()) != "" {
+
+		withoutCR := string(dropCR(buf.Bytes()))
+
 		if !p.UseTransaction {
-			p.Statements = append(p.Statements, splitStatementsBySemicolon(buf)...)
+			p.Statements = append(p.Statements, splitStatementsBySemicolon(withoutCR)...)
 		} else {
-			p.Statements = append(p.Statements, buf.String())
+			p.Statements = append(p.Statements, withoutCR)
 		}
 	}
 
 	return p, nil
+}
+
+func scanLines(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := bytes.IndexByte(data, '\n'); i >= 0 {
+		// We have a full newline-terminated line.
+		return i + 1, data[0 : i+1], nil
+	}
+	// If we're at EOF, we have a final, non-terminated line. Return it.
+	if atEOF {
+		return len(data), data, nil
+	}
+	// Request more data.
+	return 0, nil, nil
+}
+
+func dropCR(data []byte) []byte {
+	if len(data) > 0 && data[len(data)-1] == '\r' {
+		return data[0 : len(data)-1]
+	}
+	return data
 }
